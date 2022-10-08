24e6fe7699e9387f7f54614c6fe533bb7605565a
imeabdullah <abdullahprime98@gmail.com>
2022-10-08 16:17:31 +0600

    swift i/o

---
+++ main.swift
@@ -0,0 +1,12 @@
+import Swift
+
+// for print
+print("Hello World!")
+
+// print function with seperator and terminator
+print("New Year", 2022, "See you soon!", separator: ". " , terminator: "\n")
+
+// input from console
+print("Enter Your Name:", terminator: " ")
+var name = readLine()
+print("Your name is \(name!).")
